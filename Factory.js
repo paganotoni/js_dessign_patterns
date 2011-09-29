dc9dc71301010631fb0d20acfef3358429049b9d
Antonio Pagano <antonio.pagano@koombea.com>
2011-09-29 17:26:35 -0500

    [fixing] redoing factory

---
+++ Factory.js
@@ -0,0 +1,30 @@
+
+
+
+var bicycle = function(){
+  return {
+    passengers: 1, 
+    size_in_m2: 2,
+    value_in_dollars: 200
+  }
+}
+
+var car = function(){
+  return {
+    passengers: 5, 
+    size_in_m2: 8,
+    value_in_dollars: 3000
+  }
+}
+
+var bicycle = function(){
+  return {
+    passengers: 20, 
+    size_in_m2: 30,
+    value_in_dollars: 20000
+  }
+}
+
+// TODO: implement factory
+
+
