7a93276dd218d3b1da431c93f8b37249f0049d10
Antonio Pagano <antonio.pagano@koombea.com>
2011-09-30 10:19:48 -0500

    [refactor] refactoring over singleton design pattern

--- Factory.js
+++ Factory.js
@@ -1,34 +1,38 @@
 
-
-
-var mean = function( people, val, name ){
-  return {
-    passengers: people,
-    value: val, 
-    identify: function(){
-      console.info( "It's a " + name );
-    }
-  }
-}
-
-
 var mean_factory = (function(){
-
-  return{
+    // Could come form online calc or WS.    
+    var bicicle_value = 100 ;
+    var car_value = 20000 ;
+    var bus_value = 100000 ; 
+    
+    var mean = function( people, val, name ){
+      return {
+        passengers: people,
+        value: val, 
+        identify: function(){
+          console.info( "It's a " + name + " you have to pay "+ val );
+        }
+      }
+    }
+        
+   return{
     build_mean: function( passengers, value ){
+      
+        
       // This could be a more complex job
       // related to instances creation.
-      if( passengers <= 1 && value <= 200 ){
-        return new mean( passengers, value, "Bicicle" );
-      }else if( passengers <= 5 && value <= 3000 ){
-        return new mean( passengers, value, "Car" )
+      if( passengers <= 1  ){
+        return new mean( passengers, bicicle_value, "Bicicle" );
+      }else if( passengers <= 5 ){
+        return new mean( passengers, car_value, "Car" )
       }else{
-        return new mean( passengers, value, "Bus");
+        return new mean( passengers, bus_value, "Bus");
       }
     }
   }
 })()
 
-console.info( mean_Factory.build_mean( 1, 100 ).identify( ) );
-
+console.info( mean_factory.build_mean( 1 ).identify( ) );
+console.info( mean_factory.build_mean( 2 ).identify( ) );
+console.info( mean_factory.build_mean( 12 ).identify( ) );
 
