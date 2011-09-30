7de1f965f31f5179c9f0eed4d4732a1e3c4460e6
Antonio Pagano <antonio.pagano@koombea.com>
2011-09-30 10:41:06 -0500

    [refactoring] command pattern

--- Command.js
+++ Command.js
@@ -1,22 +1,40 @@
+var command_stack = []; 
 
-var UserManager = {
-  requestInfo: 
-    function( user ){
-      // Could be determined 
-      console.info( user["name"] );            
-    },
-  deleteComment:
-    function( user ){
-      console.info( "Deleting "+user["name"]+" comment" );
-    },
-  pingUser: 
-    function( user ){
-      console.info( "Pinging "+user["name"] );
+var UserManager = (function(){
+    return {
+       requestInfo: function( user ){
+          // Could be determined 
+           console.info( "checking info for : " + user  );            
+       },
+       followUser: function( user ){
+          console.info( "Following "+user+" comment" );
+       },
+       pingUser: function( user ){
+          console.info( "Checking for "+user );
+       }
     }
-}
+})();
 
-UserManager.execute = function(command){
-  return UserManager[command.request](command.user);
+
+UserManager.execute = function(command ){
+  return UserManager[command.request](command["user"]);
 };
 
-UserManager.execute( { request: "pingUser", user: user } )
+(function(){
+    if( command_stack.length > 0 ){
+        command = command_stack.pop();
+        UserManager.execute( command );
+    }
+    setTimeout(arguments.callee, 1000);
+})();
+
+command_stack.push( { request: "pingUser", user: "apagano" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "followUser", user: "apaganobeleno" } );
+command_stack.push( { request: "requestInfo", user: "apaganobeleno" } );
+
